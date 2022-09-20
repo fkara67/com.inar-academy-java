4322adda0cb012feb184bf6f714f5b95b0549c48
Fatih Kara <fkara1667@gmail.com>
2022-09-20 10:53:01 +0300

    newStartWithInteliJ

---
+++ com.inar-academy-java/src/chapters/chapter_05/Exercises05_01.java
@@ -0,0 +1,30 @@
+package chapters.chapter_05;
+
+import java.util.Scanner;
+
+public class Exercises05_01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int positive = 0, negative = 0, total = 0, count = 0, number = -1;
+        float average;
+        while (number != 0) {
+            System.out.println("Enter an integer: ");
+            number = input.nextInt();
+            total += number;
+            if (number != 0) {
+                count++;
+            }
+
+            if (number > 0) {
+                positive++;
+            } else if (number < 0) {
+                negative++;
+            }
+        }
+        average = (float) (total / count);
+        System.out.println("The number of positives is " + positive + "\nThe number of negatives is " + negative
+                + "\nTotal is " + total + "\nAverage is " + average);
+    }
+
+}
