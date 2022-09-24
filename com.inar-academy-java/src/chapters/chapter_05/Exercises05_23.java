86a509dd46eae3397c813d37a98ea12e592f8b40
Fatih Kara <fkara1667@gmail.com>
2022-09-24 23:54:16 +0300

    chapter5addition

--- com.inar-academy-java/src/chapters/chapter_05/Exercises05_23.java
+++ com.inar-academy-java/src/chapters/chapter_05/Exercises05_23.java
@@ -2,16 +2,16 @@ package chapters.chapter_05;
 
 public class Exercises05_23 {
     public static void main(String[] args) {
-        double sum = 0;
-        double sum1 = 0;
+        double sumLargeToSmall = 0;
+        double sumSmallToLarge = 0;
         for (double i = 50000.0; i > 0; i--) {
-            sum += 1/i;
+            sumLargeToSmall += 1/i;
         }
-        System.out.println(sum);
+        System.out.println("Large to small sum is " + sumLargeToSmall);
 
         for (double i = 1; i <= 50000.0; i++) {
-            sum1 += 1/i;
+            sumSmallToLarge += 1/i;
         }
-        System.out.println(sum1);
+        System.out.println("Small to large sum is " +  sumSmallToLarge);
     }
 }
