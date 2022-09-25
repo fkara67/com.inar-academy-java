348a629a1dc848302c474d8e068f38d5950c798f
Fatih Kara <fkara1667@gmail.com>
2022-09-25 11:35:46 +0300

    chapter5addition

--- com.inar-academy-java/src/chapters/chapter_05/Exercises05_24.java
+++ com.inar-academy-java/src/chapters/chapter_05/Exercises05_24.java
@@ -9,6 +9,6 @@ public class Exercises05_24 {
         for (double i = 1; i <= 97; i += 2) {
             sum += i/(i+2);
         }
-        System.out.println("Sum is " + sum);
+        System.out.println("Sum of series of 1/3 + 3/5 + ....95/97 + 97/99 is \n" + sum);
     }
 }
