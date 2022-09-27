82ab933a40d2ac249bb643dfc7002ca86be42555
Fatih Kara <fkara1667@gmail.com>
2022-09-28 01:34:09 +0300

    additionToChapter6

---
+++ com.inar-academy-java/src/chapters/chapter_06/Listing_06_04.java
@@ -0,0 +1,17 @@
+package chapters.chapter_06;
+
+public class Listing_06_04 {
+
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println("Before the invocation x is " + x);
+        increment(x);
+        System.out.println("After the invocation x is " + x);
+    }
+
+    private static void increment(int n) {
+        n++;
+        System.out.println("Inside the method n is " + n);
+    }
+
+}
