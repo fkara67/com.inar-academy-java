20109f74eebd4b9ff4d5b315ea8e75ef75a88317
Fatih Kara <fkara1667@gmail.com>
2022-09-30 00:06:01 +0300

    chapter6Exercises

--- com.inar-academy-java/src/chapters/chapter_06/Exercises_06_11.java
+++ com.inar-academy-java/src/chapters/chapter_06/Exercises_06_11.java
@@ -5,11 +5,29 @@ public class Exercises_06_11 {
         System.out.println("Sales Amount\t\tCommission");
         System.out.println("--------------------------------");
 
-        for (int salesAmount = 10000; salesAmount <= 100000; i += 5000) {
-            System.out.printf("%10s%10.2f", salesAmount,computeCommission(salesAmount));
+        for (int salesAmount = 10000; salesAmount <= 100000; salesAmount += 5000) {
+            System.out.printf("%-18s%10.2f", salesAmount,computeCommission(salesAmount));
+            System.out.println();
         }
     }
     public static double computeCommission(double salesAmount) {
+        double commission1 = 0, commission2 = 0, commission3 = 0;
+        if (salesAmount >= 0.01 && salesAmount <= 5000)
+            commission1 = salesAmount * 8 / 100;
+        if (salesAmount >= 5000.01 && salesAmount <= 10000) {
+            commission1 = 5000 * 8 / 100;
+            commission2 = (salesAmount - 5000) * 10 / 100;
+        }
+        if (salesAmount >= 10000.01) {
+            commission1 = 5000 * 8 / 100;
+            commission2 = 5000 * 10 / 100;
+            commission3 = (salesAmount - 10000) * 12 / 100;
+        }
+        double commission = commission1 + commission2 + commission3;
+
+        return commission;
+
+
 
     }
 }
