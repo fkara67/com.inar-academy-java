13858937e65ef5cbc1d30a67cf157f988e3496ca
Fatih Kara <fkara1667@gmail.com>
2022-10-03 17:20:49 +0300

    chapter6Exercises

--- com.inar-academy-java/src/chapters/chapter_06/Exercises_06_30.java
+++ com.inar-academy-java/src/chapters/chapter_06/Exercises_06_30.java
@@ -8,50 +8,58 @@ public class Exercises_06_30 {
 
     }
     public static void playCrasp() {
+    getResult(rollTwoDices());
+
+
+    }
+    public static int rollDice() {
+        return (int)(Math.random() * 6) + 1;
+    }
+    public static void rollTillWinOrLose(int sum) {
+
+        while (sum != 7 || sum != sum) {
+            System.out.println("Continue roll. Please enter any character to roll.");
+            Scanner input = new Scanner(System.in);
+            String start = input.next();
+            int dice1 = rollDice();
+            int dice2 = rollDice();
+            System.out.println("You rolled " + dice1 + " + " + dice2 + " = " + (dice1 + dice2));
+
+            if (dice1 + dice2 == 7) {
+                System.out.println("You lose!");
+                return;
+            }
+            else if (dice1 + dice2 == sum) {
+                System.out.println("You win!");
+                return;
+            }
+
+        }
+
+    }
+    public static int rollTwoDices() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter any character to roll the dice.");
         String start = input.next();
 
-        int dice1 = (int)(Math.random() * 6) + 1;
-        int dice2 = (int)(Math.random() * 6) + 1;
+        int dice1 = rollDice();
+        int dice2 = rollDice();
         int sum = dice1 + dice2;
+        System.out.println("You rolled " + dice1 + " + " + dice2 + " = " + sum);
+        return sum;
+
+    }
+    public static void getResult(int sum) {
         if (sum == 2 || sum == 3 || sum == 12) {
-            System.out.println("You rolled " + dice1 + " + " + dice2 + " = " + sum);
             System.out.println("You lose!");
             return;
         }
         else if (sum == 7 || sum == 11) {
-            System.out.println("You rolled " + dice1 + " + " + dice2 + " = " + sum);
             System.out.println("You win!");
         }
         else {
-            System.out.println("You rolled " + dice1 + " + " + dice2 + " = " + sum);
             System.out.println("The point is " + sum);
-
-
-            while (dice1 + dice2 != 7 || dice1 + dice2 != sum) {
-                System.out.println("Continue roll. Please enter any character to roll.");
-                Scanner input3 = new Scanner(System.in);
-                String start3 = input3.next();
-                dice1 = (int)(Math.random() * 6) + 1;
-                dice2 = (int)(Math.random() * 6) + 1;
-
-                if (dice1 + dice2 == 7) {
-                    System.out.println("You rolled " + dice1 + " + " + dice2 + " = " + (dice1 + dice2));
-                    System.out.println("You lose!");
-                    return;
-                }
-                else if (dice1 + dice2 == sum) {
-                    System.out.println("You rolled " + dice1 + " + " + dice2 + " = " + (dice1 + dice2));
-                    System.out.println("You win!");
-                    return;
-                }
-                else {
-                    System.out.println("You rolled " + dice1 + " + " + dice2 + " = " + (dice1 + dice2));
-                }
-            }
-
-
+            rollTillWinOrLose(sum);
         }
 
     }
