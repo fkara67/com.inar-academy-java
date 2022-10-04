c53444c8e50a3b7ab2be8f53af98823e88038ce9
Fatih Kara <fkara1667@gmail.com>
2022-10-04 23:11:12 +0300

    chapter6Exercises

--- com.inar-academy-java/src/chapters/chapter_06/Exercises_06_32.java
+++ com.inar-academy-java/src/chapters/chapter_06/Exercises_06_32.java
@@ -1,54 +1,43 @@
 package chapters.chapter_06;
 
-import java.util.Scanner;
-
 public class Exercises_06_32 {
     public static void main(String[] args) {
         int count = 0;
         int win = 0;
-        while (count < 3) {
-            playCrasp(win);
+        while (count < 10000) {
+            win = playCrasp(win);
             count++;
         }
-        System.out.println("win is ");
-
+        System.out.println("win is " + win);
     }
-    public static void playCrasp(int win1) {
-        getResult(rollTwoDices(),win1);
-        System.out.println(getResult(rollTwoDices(),win1));
-    }
-    public static int rollTwoDices() {
+    public static int playCrasp(int win2) {
         int dice1 = (int)(Math.random() * 6) + 1;
         int dice2 = (int)(Math.random() * 6) + 1;
+        int sum = dice1 + dice2;
         System.out.println("You rolled " + dice1 + " + " + dice2 + " = " + (dice1 + dice2));
-        return dice1 + dice2;
-
-    }
-    public static int getResult(int sum, int win2) {
 
         if (sum == 2 || sum == 3 || sum == 12) {
             System.out.println("You lose!");
-
         }
         else if (sum == 7 || sum == 11) {
             System.out.println("You win!");
             win2++;
-
         }
-        else {
+        else if (sum == 4 || sum == 5 || sum == 6 || sum == 8 || sum == 9 || sum == 10) {
             System.out.println("The point is " + sum);
-            for (int dice = rollTwoDices(); dice != 7 || dice != sum;) {
-                if (dice == 7) {
+            while (sum != 7)  {
+                dice1 = (int)(Math.random() * 6) + 1;
+                dice2 = (int)(Math.random() * 6) + 1;
+                System.out.println("You rolled " + dice1 + " + " + dice2 + " = " + (dice1 + dice2));
+                if (dice1 + dice2 == 7) {
                     System.out.println("You lose!");
                     break;
                 }
-                else if (dice == sum) {
+                else if (dice1 + dice2 == sum) {
                     System.out.println("You win!");
                     win2++;
                     break;
-
                 }
-
             }
         }
         return win2;
