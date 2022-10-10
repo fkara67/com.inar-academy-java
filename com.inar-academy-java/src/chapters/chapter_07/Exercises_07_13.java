b06efb16867350864b98c607228820b57518936e
Fatih Kara <fkara1667@gmail.com>
2022-10-10 16:56:46 +0300

    arrayExercises

--- com.inar-academy-java/src/chapters/chapter_07/Exercises_07_13.java
+++ com.inar-academy-java/src/chapters/chapter_07/Exercises_07_13.java
@@ -1,15 +1,24 @@
 package chapters.chapter_07;
 
+import java.util.Scanner;
+
 public class Exercises_07_13 {
     public static void main(String[] args) {
-    int[] numbers;
-    getRandom();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter ");
+        int[] numbers = new int[10];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = input.nextInt();
+        }
+        System.out.println("The random number is " + getRandom(numbers));
     }
     public static int getRandom(int...numbers) {
         int random = (int)(Math.random() * 53) + 1;
         for (int i = 0; i < numbers.length; i++) {
-            random = (int)(Math.random() * 53) + 1;
-            numbers[i] = random;
+            if (random == numbers[i]) {
+                random = (int)(Math.random() * 53) + 1;
+                i = -1;
+            }
         }
         return random;
     }
--- com.inar-academy-java/src/chapters/chapter_07/Exercises_07_14.java
+++ com.inar-academy-java/src/chapters/chapter_07/Exercises_07_14.java
@@ -9,14 +9,15 @@ public class Exercises_07_14 {
         int[] numbers = new int[5];
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = input.nextInt();
-            int gcd = gcd(numbers);
-            System.out.println("gcd of number " + numbers[i] + " is " + gcd);
+
+            System.out.println("gcd of number " + numbers[i] + " is " + gcd(numbers));
         }
 
     }
     public static int gcd(int... numbers) {
         int gcd = 0;
-        for (int j = numbers[numbers.length] - 1; j < numbers[j] / 2; j--) {
+        int i = 0;
+        for (int j = numbers[i] - 1; j < numbers[i] / 2; j--, i++) {
             if (numbers[j] % j == 0) {
                 gcd = j;
                 break;
