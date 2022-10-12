62cdccc23f621794ccc283876e3ac821d3194796
Fatih Kara <fkara1667@gmail.com>
2022-10-12 17:17:04 +0300

    arrayExercises

--- com.inar-academy-java/src/chapters/chapter_07/Exercises_07_18.java
+++ com.inar-academy-java/src/chapters/chapter_07/Exercises_07_18.java
@@ -1,12 +1,18 @@
 package chapters.chapter_07;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercises_07_18 {
     public static void main(String[] args) {
         final int NUMBER_OF_INPUTS = 10;
         System.out.print("Enter " + NUMBER_OF_INPUTS + " double values: ");
+
         double[] numbers = createArrayWithInput(NUMBER_OF_INPUTS);
+
+        bubbleSort(numbers);
+
+        System.out.print(Arrays.toString(numbers));
     }
     public static double[] createArrayWithInput(int numberOfInputs) {
         Scanner input = new Scanner(System.in);
@@ -17,6 +23,15 @@ public class Exercises_07_18 {
         return numbers;
     }
     public static void bubbleSort(double[] list) {
+        for (int j = 0; j < list.length; j++) {
+            for (int i = 0; i + 1 < list.length; i++) {
+                if (list[i] > list[i + 1]) {
+                    double temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
 
+                }
+            }
+        }
     }
 }
---
+++ com.inar-academy-java/src/chapters/chapter_07/Exercises_07_23.java
@@ -0,0 +1,21 @@
+package chapters.chapter_07;
+
+public class Exercises_07_23 {
+    public static void main(String[] args) {
+        boolean[] isLocked = new boolean[100];
+
+        for (int i = 0; i < isLocked.length; i++) {
+            for (int j = i; j < isLocked.length; j++) {
+                if (isLocked[i]) {
+                    isLocked[i] = false;
+                }
+                else isLocked[i] = true;
+            }
+        }
+        for (int i = 0; i < isLocked.length; i++) {
+            if (isLocked[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
