220a0f4925c8f8c19b7c64707cf3cd45031ed821
Fatih Kara <fkara1667@gmail.com>
2022-10-19 16:50:55 +0300

    learningMultiDimensionalArrays

--- com.inar-academy-java/src/chapters/chapter_08/CheckSudokuSolution.java
+++ com.inar-academy-java/src/chapters/chapter_08/CheckSudokuSolution.java
@@ -6,7 +6,7 @@ public class CheckSudokuSolution {
     public static void main(String[] args) {
         int[][] grid = readSolution();
 
-        System.out.println(isValid(grid) ? "Valid Solution" : "Invalid Solution");
+        System.out.println(isSolutionValid(grid) ? "Valid Solution" : "Invalid Solution");
     }
     public static int[][] readSolution() {
         Scanner input = new Scanner(System.in);
@@ -19,41 +19,37 @@ public class CheckSudokuSolution {
         }
         return grid;
     }
-    public static boolean isValid(int[][] grid) {
+    public static boolean isSolutionValid(int[][] grid) {
         for (int i = 0; i < 9; i++) {
-            if(!isValid(grid, i)) {
-                return false;
+            for (int j = 0; j < 9; j++) {
+                if ((grid[i][j] < 1 || grid[i][j] > 9) || !isValid(grid, i , j)) {
+                    return false;
+                }
             }
         }
         return true;
     }
-    public static boolean isValid(int[][] grid, int i) {
-        boolean[] isUnique = new boolean[9];
+    public static boolean isValid(int[][] grid, int i, int j) {
         for (int col = 0; col < 9; col++) {
-            if (grid[i][col] < 1 || grid[i][col] > 9) {
+            if (col != j && grid[i][col] == grid[i][j]) {
                 return false;
             }
-            isUnique[grid[i][col]] = true;
-        }
-        if (!isUnique(isUnique)) {
-            return false;
         }
         for (int row = 0; row < 9; row++) {
-            isUnique[grid[row][i]] = true;
-        }
-        for (int j = 0; j < 18; j++) {
-            if (!isUnique[j]) {
+            if (row != i && grid[row][j] == grid[i][j]) {
                 return false;
+            }
+        }
+        for (int row = (i / 3) * 3; row < (i / 3) * 3 + 3; row++) {
+            for (int col = (j / 3) * 3; col < (j / 3) * 3 + 3; col++) {
+                if (row != i && col != j && grid[i][j] == grid[row][col]) {
+                    return false;
+                }
             }
         }
         return true;
     }
-    public static boolean isUnique(boolean[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            if (!arr[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
+
+
+
 }
