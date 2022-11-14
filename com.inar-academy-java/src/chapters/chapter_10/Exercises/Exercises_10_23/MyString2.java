4232d8a88df9cf38d643e58d03563db20a364316
Fatih Kara <fkara1667@gmail.com>
2022-11-14 23:43:24 +0300

    ObjectOreentingThinking

--- com.inar-academy-java/src/chapters/chapter_10/Exercises/Exercises_10_23/MyString2.java
+++ com.inar-academy-java/src/chapters/chapter_10/Exercises/Exercises_10_23/MyString2.java
@@ -1,18 +1,50 @@
 package chapters.chapter_10.Exercises.Exercises_10_23;
 
+import java.util.Arrays;
+
 public class MyString2 {
     private String s;
+    private char[] chars;
 
     public MyString2(String s) {
+        chars = s.toCharArray();
         this.s = s;
     }
     public int compareTo(String s) {
-
-        for (int i = 0; i < s.length(); i++) {
+        int length = Math.min(s.length(), this.s.length());
+        for (int i = 0; i < length; i++) {
             if (this.s.charAt(i) != s.charAt(i)) {
                 return this.s.charAt(i) - s.charAt(i);
             }
         }
-        return 0;
+        if (this.s.length() == s.length())
+            return 0;
+        return (this.s.length() > s.length()) ? 1 : -1;
+    }
+    public MyString2 substring(int begin) {
+        char[] chars = new char[this.chars.length - begin];
+
+        for (int i = begin, j = 0; i < this.chars.length; i++, j++) {
+            chars[j] = this.chars[i];
+        }
+        return new MyString2(Arrays.toString(chars));
+    }
+    public MyString2 toUpperCase() {
+        char[] ch = new char[chars.length];
+
+        for (int i = 0; i < ch.length; i++) {
+            if (chars[i] < 'z' && chars[i] > 'a') {
+                ch[i] = (char) (chars[i] - ('a' - 'A'));
+            }
+            else
+                ch[i] = chars[i];
+        }
+        return new MyString2(Arrays.toString(ch));
+    }
+    public char[] toChars() {
+        return chars;
+    }
+    public static MyString2 valueOf(boolean b) {
+        return new MyString2(b ? "true" : "false");
     }
 }
---
+++ com.inar-academy-java/src/chapters/chapter_10/Exercises/Exercises_10_23/TestMyString2.java
@@ -0,0 +1,10 @@
+package chapters.chapter_10.Exercises.Exercises_10_23;
+
+public class TestMyString2 {
+    public static void main(String[] args) {
+        MyString2 m = new MyString2("FATIH");
+        System.out.println(m.compareTo("FATIHK"));
+
+
+    }
+}
